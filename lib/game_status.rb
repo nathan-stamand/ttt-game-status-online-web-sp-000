d319d03e6cbd2a59244231b2698f7113af64def0
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-13 15:24:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,13 +5,13 @@ end
 
 def won?(board)
   
-  i = 7
   
+  
+  i = 7
   while i < 9
     win_position_1 = WIN_COMBINATIONS[i][0]
     win_position_2 = WIN_COMBINATIONS[i][1]
     win_position_3 = WIN_COMBINATIONS[i][2]
-    
     position_1 = board[WIN_COMBINATIONS[i][0]]
     position_2 = board[WIN_COMBINATIONS[i][1]]
     position_3 = board[WIN_COMBINATIONS[i][2]]
