ec8556224eb3d3d41a21e47a81d4b3c8768ddf1f
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-13 15:56:27 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,31 +1,33 @@
+require 'pry'
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]]
+  
 def won?(board)
   
   i = 0
   
-  while i < 9
-  
-    win_position_1 = WIN_COMBINATIONS[i][0]
-    win_position_2 = WIN_COMBINATIONS[i][1]
-    win_position_3 = WIN_COMBINATIONS[i][2]
+  while i < 8
+    
     position_1 = board[WIN_COMBINATIONS[i][0]]
     position_2 = board[WIN_COMBINATIONS[i][1]]
     position_3 = board[WIN_COMBINATIONS[i][2]]
-  
+    
     if [position_1, position_2, position_3] == ["X", "X", "X"]
       return WIN_COMBINATIONS[i]
-      
+
     elsif [position_1, position_2, position_3] == ["O", "O", "O"]
       return WIN_COMBINATIONS[i]
-      
-    elsif board.all? {|i| i == " "}
-      return false
-      
-    elsif board.none? {|i| i == " "}
-      return nil
       
     else
       i += 1
@@ -40,16 +42,8 @@ end
 
 def draw?(board)
   
-  if won?(board)
-    return false
-    
-  elsif won?(board) == nil
-    return true
-    
-  else
-    return false
-    
-  end
+full?(board) && !won?(board)
+
 end
 
 def over?(board)
@@ -68,28 +62,8 @@ end
 
 def winner(board)
   
-  number = board.select do |i|
-    i == "X"
-    i == "O"
-  end
-  
-  if won?(board) != false
-    if number.count % 2 == 1 
-      return "O"
-    else
-      return "X"
-    end
-  elsif draw?(board)
-    return nil
+  if combo = won?(board)
+    board[combo[0]]
   end
 end
 
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]]
