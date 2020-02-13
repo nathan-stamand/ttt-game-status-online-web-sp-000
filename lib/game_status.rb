5c89fa8d9be976ca5820dc4727bcd3c77deccbce
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-13 15:48:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,11 +29,11 @@ def won?(board)
     elsif [position_1, position_2, position_3] == ["O", "O", "O"]
       return WIN_COMBINATIONS[i]
       
-    elsif board.all? {|i| i == " "}
-      return false
+    # elsif board.all? {|i| i == " "}
+    #   return false
       
-    elsif board.none? {|i| i == " "}
-      return nil
+    # elsif board.none? {|i| i == " "}
+    #   return nil
       
     else
       i += 1
